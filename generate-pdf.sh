feabec0a9a8ad404cd0ac175392ff722afae3a9b
André Formento <andreformento.sc@gmail.com>
2017-11-14 19:26:46 -0200

    Volta arquivo de geracao de pdf

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -4,22 +4,20 @@ rm -f monografia.pdf
 
 cd tex
 
-docker run -it -v "$PWD":/data -w /data andreformento/latex       \
-    rm -rf *.log                                               && \
-    pdflatex monografia.tex                                    && \
-    bibtex monografia.aux                                      && \
-    makeindex monografia.idx                                   && \
-  # makeindex monografia.nlo -s nomencl.ist -o monografia.nls  && \
-    pdflatex monografia.tex                                    && \
-    pdflatex monografia.tex
+# docker run -it -v "$PWD":/data -w /data andreformento/latex       \
+#     rm -rf *.log                                               && \
+#     pdflatex monografia.tex                                    && \
+#     bibtex monografia.aux                                      && \
+#     makeindex monografia.idx                                   && \
+#   # makeindex monografia.nlo -s nomencl.ist -o monografia.nls  && \
+#     pdflatex monografia.tex                                    && \
+#     pdflatex monografia.tex
 
-# docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
-# docker run -it -v "$PWD":/data -w /data andreformento/latex bibtex monografia
-# docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
-# 
-# docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
-# docker run -it -v "$PWD":/data -w /data andreformento/latex bibtex monografia
-# docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
+docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
+docker run -it -v "$PWD":/data -w /data andreformento/latex bibtex monografia.aux
+docker run -it -v "$PWD":/data -w /data andreformento/latex makeindex monografia.tex
+docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
+docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
 
 mv monografia.pdf ../
 
