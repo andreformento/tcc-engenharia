aecfb276265f07bc0c9065dbf83fa439f5b9d47b
André Formento <andreformento.sc@gmail.com>
2017-11-17 17:19:26 -0200

    Referências automáticas quando não houver citação

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -4,6 +4,13 @@ rm -f monografia.pdf
 
 cd tex
 
+rm -rf referencias-bibliograficas-sem-citacao.tex
+
+grep '^ *@.*{.*,' referencias-bibliograficas.bib > referencias-bibliograficas-sem-citacao.tex
+sed -i 's/,/}/g' referencias-bibliograficas-sem-citacao.tex
+sed -i 's/@.*{/\\nocite{/g' referencias-bibliograficas-sem-citacao.tex
+
+
 # docker run -it -v "$PWD":/data -w /data andreformento/latex       \
 #     rm -rf *.log                                               && \
 #     pdflatex monografia.tex                                    && \
