12f90cc8be65771410dadbc3121ea4e9f458d112
andreformento <andreformento.sc@gmail.com>
2017-11-21 19:06:07 +0000

    Internaliza geracao das referencias de arquivos

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -4,22 +4,13 @@ rm -f monografia.pdf
 
 cd tex
 
+# gerar arquivo de referências sem citações
 rm -rf referencias-bibliograficas-sem-citacao.tex
+docker run --rm -it -v "$PWD":/data -w /data andreformento/latex grep '^ *@.*{.*,' referencias-bibliograficas.bib > referencias-bibliograficas-sem-citacao.tex
+docker run --rm -it -v "$PWD":/data -w /data andreformento/latex sed -i 's/,/}/g' referencias-bibliograficas-sem-citacao.tex
+docker run --rm -it -v "$PWD":/data -w /data andreformento/latex sed -i 's/@.*{/\\nocite{/g' referencias-bibliograficas-sem-citacao.tex
 
-grep '^ *@.*{.*,' referencias-bibliograficas.bib > referencias-bibliograficas-sem-citacao.tex
-sed -i 's/,/}/g' referencias-bibliograficas-sem-citacao.tex
-sed -i 's/@.*{/\\nocite{/g' referencias-bibliograficas-sem-citacao.tex
-
-
-# docker run -it -v "$PWD":/data -w /data andreformento/latex       \
-#     rm -rf *.log                                               && \
-#     pdflatex monografia.tex                                    && \
-#     bibtex monografia.aux                                      && \
-#     makeindex monografia.idx                                   && \
-#   # makeindex monografia.nlo -s nomencl.ist -o monografia.nls  && \
-#     pdflatex monografia.tex                                    && \
-#     pdflatex monografia.tex
-
+# gerar PDF de maneira personalizada
 docker run --rm -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synctex=1 -interaction=nonstopmode monografia.tex
 docker run --rm -it -v "$PWD":/data -w /data andreformento/latex bibtex monografia.aux
 docker run --rm -it -v "$PWD":/data -w /data andreformento/latex makeindex monografia.tex
