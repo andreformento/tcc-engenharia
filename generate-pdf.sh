b4a731802030d7270a9e63992a253e61a7c1b146
André Formento <andreformento.sc@gmail.com>
2017-11-30 09:53:51 -0200

    Remove auto conversão que não funciona - script

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 rm -f monografia.pdf
-rm -f monografia.doc
-rm -f monografia.docx
+# rm -f monografia.doc
+# rm -f monografia.docx
 
 cd tex
 
@@ -21,12 +21,12 @@ docker run --rm -it -v "$PWD":/data -w /data andreformento/latex pdflatex -synct
 
 # docker run --rm -it -v "$PWD":/data -w /data andreformento/latex pandoc -t docx -f latex -o monografia.docx monografia.tex
 # docker run --rm -it -v "$PWD":/data -w /data andreformento/latex abiword --to=doc monografia.pdf
-docker run --rm -it -v "$PWD":/data -w /data andreformento/latex soffice --infilter="writer_pdf_import" --convert-to doc monografia.pdf
-docker run --rm -it -v "$PWD":/data -w /data andreformento/latex soffice --infilter="writer_pdf_import" --convert-to docx monografia.pdf
+# docker run --rm -it -v "$PWD":/data -w /data andreformento/latex soffice --infilter="writer_pdf_import" --convert-to doc monografia.pdf
+# docker run --rm -it -v "$PWD":/data -w /data andreformento/latex soffice --infilter="writer_pdf_import" --convert-to docx monografia.pdf
 
 mv monografia.pdf ../
-mv monografia.doc ../
-mv monografia.docx ../
+# mv monografia.doc ../
+# mv monografia.docx ../
 
 cd ..
 ./clear.sh
